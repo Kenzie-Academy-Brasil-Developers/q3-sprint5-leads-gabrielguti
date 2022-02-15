e39e18b2665e8433d9c82e5b3476467ab71cd0c1
Gabriel Gutierez <gabriel@gabriel-inspiron-14-3467>
2022-02-15 16:41:47 -0300

    fix: message on get/leads

--- app/models/exceptions.py
+++ app/models/exceptions.py
@@ -2,7 +2,7 @@ class InvalidPhone(Exception):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.code = 400
-        self.message = {'error': 'phone must follow this format: (xx)xxxxx-xxxx'}
+        self.message = {'error': 'Phone must follow this format: (xx)xxxxx-xxxx'}
 
 
 class InvalidValue(Exception):
@@ -15,4 +15,4 @@ class NothingHere(Exception):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.code = 200
-        self.message = []
+        self.message = {"message":"There's nothing here"}
